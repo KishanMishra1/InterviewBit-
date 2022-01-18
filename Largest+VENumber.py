7fdffa1b0dcee7df91b70266ca86fefaba20a1d3
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-19 00:55:47 +0530

    Create Largest+VENumber.py

---
+++ Largest+VENumber.py
@@ -0,0 +1,6 @@
+def largestNumber(self, A):
+        a=map(str,A)
+        key=cmp_to_key(lambda a,b: 1 if a+b>=b+a else -1)
+        res= "".join(sorted(a,key=key,reverse=True))
+        res=res.lstrip("0")
+        return (res if res else 0)
